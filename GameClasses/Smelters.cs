12d88306c7f1f260d79d35c8b0a58f286a0c6ee1
TastyChickenLegs <118636411+tastychickenleg@users.noreply.github.com>
2023-02-04 15:26:37 -0600

    Add project files.

---
+++ GameClasses/Smelters.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using AutomaticFuel;
+using UnityEngine;
+using System.Reflection;
+
+
+namespace AutomaticFuel.GameClasses
+{
+    internal class Smelters
+    {
+        private static Dictionary<string, ItemDrop> metals = new Dictionary<string, ItemDrop>
+        {
+            { "$item_copperore", null },
+            { "$item_copper", null },
+            { "$item_ironscrap", null },
+            { "$item_iron", null },
+            { "$item_tinore", null },
+            { "$item_tin", null },
+            { "$item_silverore", null },
+            { "$item_silver", null },
+            { "$item_copperscrap", null }
+            //{ "$item_bronzescrap", null }
+        };
+
+        [HarmonyPatch(typeof(Smelter), nameof(Smelter.Awake))]
+        //allows the use of all fuel in the Blast Furnace 
+        public class BlastFurnacePatch
+        {
+            private static void Prefix(ref Smelter __instance)
+            {
+                if (AutomaticFuel.AutomaticFuelPlugin.configBlastFurnaceTakesAll.Value)
+                {
+                    if (__instance.m_name != "$piece_blastfurnace")
+                    {
+                        UnityEngine.Debug.Log("Ignored non-blast furnace smelter.");
+                        return;
+                    }
+                    UnityEngine.Debug.Log("Found a blast furnace! Applying fix.");
+
+                    ObjectDB instance = ObjectDB.instance;
+                    List<ItemDrop> materials = instance.GetAllItems(ItemDrop.ItemData.ItemType.Material, "");
+
+                    foreach (ItemDrop material in materials)
+                    {
+                        if (metals.Keys.Contains(material.m_itemData.m_shared.m_name))
+                        {
+                            UnityEngine.Debug.Log("Adding " + material.m_itemData.m_shared.m_name + " to list of materials.");
+                            metals[material.m_itemData.m_shared.m_name] = material;
+                        }
+                    }
+
+                    List<Smelter.ItemConversion> conversions = new List<Smelter.ItemConversion>()
+                        {
+                            new Smelter.ItemConversion{ m_from = metals["$item_copperore"], m_to = metals["$item_copper"]},
+                            new Smelter.ItemConversion{ m_from = metals["$item_tinore"], m_to = metals["$item_tin"]},
+                            new Smelter.ItemConversion{ m_from = metals["$item_ironscrap"], m_to = metals["$item_iron"]},
+                            new Smelter.ItemConversion{ m_from = metals["$item_silverore"], m_to = metals["$item_silver"]},
+                           new Smelter.ItemConversion{ m_from = metals["$item_copperscrap"], m_to = metals["$item_copper"]},
+                           //new Smelter.ItemConversion{ m_from = metals["$item_bronzescrap"], m_to = metals["$item_bronze"]},
+                        };
+
+                    foreach (Smelter.ItemConversion conversion in conversions)
+                    {
+                        __instance.m_conversion.Add(conversion);
+                    }
+                }
+            }
+        }
+        //allows the stacking of smelters //
+
+        [HarmonyPatch(typeof(Smelter), "UpdateSmoke")]
+        private class SmelterUpdateSmoke_Patch
+        {
+            private static void Postfix(Smelter __instance)
+            //checks to see if stack smelters is true and kills off the smoke blocked check
+            {
+                if (__instance.m_smokeSpawner != null)
+                {
+                    if (AutomaticFuelPlugin.configStackSmelters.Value)
+                    {
+                        __instance.m_smokeSpawner.enabled = false;
+                        __instance.m_blockedSmoke = false;
+                        return;
+                    }
+                }
+            }
+        }
+        
+        [HarmonyPatch(typeof(Smelter), "UpdateSmelter")]
+        private static class Smelter_FixedUpdate_Patch
+        {
+            private static void Postfix(Smelter __instance, ZNetView ___m_nview)
+            {
+                if (!Player.m_localPlayer || !AutomaticFuelPlugin.isOn.Value || ___m_nview == null || !___m_nview.IsOwner())
+                    return;
+                if (__instance.name.Contains("charcoal_kiln") && AutomaticFuelPlugin.turnOffKiln.Value)
+                    return;
+                if (__instance.name.Contains("piece_spinningwheel") && AutomaticFuelPlugin.turnOffSpinningWheel.Value)
+                    return;
+                if (__instance.name.Contains("windmill") && AutomaticFuelPlugin.turnOffWindmills.Value)
+                    return;
+
+
+                if (Time.time - AutomaticFuelPlugin.lastFuel < 0.1)
+                {
+                    AutomaticFuelPlugin.fuelCount++;
+                    RefuelSmelter(__instance, ___m_nview, AutomaticFuelPlugin.fuelCount * 33);
+                }
+                else
+                {
+                    AutomaticFuelPlugin.fuelCount = 0;
+                    AutomaticFuelPlugin.lastFuel = Time.time;
+                    RefuelSmelter(__instance, ___m_nview, 0);
+                }
+            }
+        }
+
+
+
+        public static async void RefuelSmelter(Smelter __instance, ZNetView ___m_nview, int delay)
+        {
+            await Task.Delay(delay);
+
+            if (!__instance || !___m_nview || !___m_nview.IsValid() || !AutomaticFuelPlugin.modEnabled.Value)
+                return;
+
+            int maxOre = __instance.m_maxOre - Traverse.Create(__instance).Method("GetQueueSize").GetValue<int>();
+            int maxFuel = __instance.m_maxFuel - Mathf.CeilToInt(___m_nview.GetZDO().GetFloat("fuel", 0f));
+
+            List<Container> nearbyOreContainers = TastyUtils.GetNearbyContainers
+                (__instance.transform.position, AutomaticFuelPlugin.smelterOreRange.Value);
+
+            List<Container> nearbyFuelContainers = TastyUtils.GetNearbyContainers
+                (__instance.transform.position, AutomaticFuelPlugin.smelterFuelRange.Value);
+
+            if (__instance.name.Contains("charcoal_kiln") && AutomaticFuelPlugin.restrictKilnOutput.Value)
+            {
+                string outputName = __instance.m_conversion[0].m_to.m_itemData.m_shared.m_name;
+                int maxOutput = AutomaticFuelPlugin.restrictKilnOutputAmount.Value - Traverse.Create(__instance).Method("GetQueueSize").GetValue<int>();
+                foreach (Container c in nearbyOreContainers)
+                {
+                    List<ItemDrop.ItemData> itemList = new List<ItemDrop.ItemData>();
+                    c.GetInventory().GetAllItems(outputName, itemList);
+
+                    foreach (var outputItem in itemList)
+                    {
+                        if (outputItem != null)
+                            maxOutput -= outputItem.m_stack;
+                    }
+                }
+                if (maxOutput < 0)
+                    maxOutput = 0;
+                if (maxOre > maxOutput)
+                    maxOre = maxOutput;
+            }
+
+            bool fueled = false;
+            bool ored = false;
+
+            Vector3 position = __instance.transform.position + Vector3.up;
+            foreach (Collider collider in Physics.OverlapSphere(position, AutomaticFuelPlugin.dropRange.Value, LayerMask.GetMask(new string[] { "item" })))
+            {
+                if (collider?.attachedRigidbody)
+                {
+                    ItemDrop item = collider.attachedRigidbody.GetComponent<ItemDrop>();
+                    //Dbgl($"nearby item name: {item.m_itemData.m_dropPrefab.name}");
+
+                    if (item?.GetComponent<ZNetView>()?.IsValid() != true)
+                        continue;
+
+                    string name = TastyUtils.GetPrefabName(item.gameObject.name);
+
+                    foreach (Smelter.ItemConversion itemConversion in __instance.m_conversion)
+                    {
+                        if (ored)
+                            break;
+                        if (item.m_itemData.m_shared.m_name == itemConversion.m_from.m_itemData.m_shared.m_name && maxOre > 0)
+                        {
+                            if (AutomaticFuelPlugin.oreDisallowTypes.Value.Split(',').Contains(name))
+                            {
+                                //Dbgl($"container at {c.transform.position} has {item.m_itemData.m_stack} {item.m_dropPrefab.name} but it's forbidden by config");
+                                continue;
+                            }
+
+                            AutomaticFuelPlugin.Dbgl($"auto adding ore {name} from ground");
+
+                            int amount = Mathf.Min(item.m_itemData.m_stack, maxOre);
+                            maxOre -= amount;
+
+                            for (int i = 0; i < amount; i++)
+                            {
+                                if (item.m_itemData.m_stack <= 1)
+                                {
+                                    if (___m_nview.GetZDO() == null)
+                                        AutomaticFuelPlugin.Destroy(item.gameObject);
+                                    else
+                                        ZNetScene.instance.Destroy(item.gameObject);
+                                    ___m_nview.InvokeRPC("AddOre", new object[] { name });
+                                    if (AutomaticFuelPlugin.distributedFilling.Value)
+                                        ored = true;
+                                    break;
+                                }
+
+                                item.m_itemData.m_stack--;
+                                ___m_nview.InvokeRPC("AddOre", new object[] { name });
+                                Traverse.Create(item).Method("Save").GetValue();
+                                if (AutomaticFuelPlugin.distributedFilling.Value)
+                                    ored = true;
+                            }
+                        }
+                    }
+
+                    if (__instance.m_fuelItem && item.m_itemData.m_shared.m_name ==
+                        __instance.m_fuelItem.m_itemData.m_shared.m_name && maxFuel > 0 && !fueled)
+                    {
+                        if (AutomaticFuelPlugin.fuelDisallowTypes.Value.Split(',').Contains(name))
+                        {
+                            //Dbgl($"ground has {item.m_itemData.m_dropPrefab.name} but it's forbidden by config");
+                            continue;
+                        }
+
+                        AutomaticFuelPlugin.Dbgl($"auto adding fuel {name} from ground");
+
+                        int amount = Mathf.Min(item.m_itemData.m_stack, maxFuel);
+                        maxFuel -= amount;
+
+                        for (int i = 0; i < amount; i++)
+                        {
+                            if (item.m_itemData.m_stack <= 1)
+                            {
+                                if (___m_nview.GetZDO() == null)
+                                    AutomaticFuelPlugin.Destroy(item.gameObject);
+                                else
+                                    ZNetScene.instance.Destroy(item.gameObject);
+                                ___m_nview.InvokeRPC("AddFuel", new object[] { });
+                                if (AutomaticFuelPlugin.distributedFilling.Value)
+                                    fueled = true;
+                                break;
+                            }
+
+                            item.m_itemData.m_stack--;
+                            ___m_nview.InvokeRPC("AddFuel", new object[] { });
+                            Traverse.Create(item).Method("Save").GetValue();
+                            if (AutomaticFuelPlugin.distributedFilling.Value)
+                            {
+                                fueled = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (Container c in nearbyOreContainers)
+            {
+                foreach (Smelter.ItemConversion itemConversion in __instance.m_conversion)
+                {
+                    if (ored)
+                        break;
+                    List<ItemDrop.ItemData> itemList = new List<ItemDrop.ItemData>();
+                    c.GetInventory().GetAllItems(itemConversion.m_from.m_itemData.m_shared.m_name, itemList);
+
+                    foreach (var oreItem in itemList)
+                    {
+                        if (oreItem != null && maxOre > 0 && (!AutomaticFuelPlugin.leaveLastItem.Value || oreItem.m_stack > 1))
+                        {
+                            if (AutomaticFuelPlugin.oreDisallowTypes.Value.Split(',').Contains(oreItem.m_dropPrefab.name))
+                                continue;
+                            maxOre--;
+
+                            AutomaticFuelPlugin.Dbgl($"container at {c.transform.position} has {oreItem.m_stack} {oreItem.m_dropPrefab.name}, taking one");
+
+                            ___m_nview.InvokeRPC("AddOre", new object[] { oreItem.m_dropPrefab?.name });
+                            c.GetInventory().RemoveItem(itemConversion.m_from.m_itemData.m_shared.m_name, 1);
+                            typeof(Container).GetMethod("Save", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(c, new object[] { });
+                            typeof(Inventory).GetMethod("Changed", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(c.GetInventory(), new object[] { });
+                            if (AutomaticFuelPlugin.distributedFilling.Value)
+                            {
+                                ored = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (Container c in nearbyFuelContainers)
+            {
+                if (!__instance.m_fuelItem || maxFuel <= 0 || fueled)
+                    break;
+
+                List<ItemDrop.ItemData> itemList = new List<ItemDrop.ItemData>();
+                c.GetInventory().GetAllItems(__instance.m_fuelItem.m_itemData.m_shared.m_name, itemList);
+
+                foreach (var fuelItem in itemList)
+                {
+                    if (fuelItem != null && (!AutomaticFuelPlugin.leaveLastItem.Value || fuelItem.m_stack > 1))
+                    {
+                        maxFuel--;
+                        if (AutomaticFuelPlugin.fuelDisallowTypes.Value.Split(',').Contains(fuelItem.m_dropPrefab.name))
+                        {
+                            //Dbgl($"container at {c.transform.position} has {item.m_stack} {item.m_dropPrefab.name} but it's forbidden by config");
+                            continue;
+                        }
+
+                        AutomaticFuelPlugin.Dbgl($"container at {c.transform.position} has {fuelItem.m_stack} {fuelItem.m_dropPrefab.name}, taking one");
+
+                        ___m_nview.InvokeRPC("AddFuel", new object[] { });
+
+                        c.GetInventory().RemoveItem(__instance.m_fuelItem.m_itemData.m_shared.m_name, 1);
+                        typeof(Container).GetMethod("Save", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(c, new object[] { });
+                        typeof(Inventory).GetMethod("Changed", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(c.GetInventory(), new object[] { });
+                        if (AutomaticFuelPlugin.distributedFilling.Value)
+                        {
+                            fueled = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ServerSync;
+using UnityEngine;
+
+namespace AutomaticFuel
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class AutomaticFuelPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "AutomaticFuel";
+        internal const string ModVersion = "1.3.1";
+        internal const string Author = "TastyChickenLegs";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        private static readonly bool isDebug = false;
+
+        public static ConfigEntry<float> dropRange;
+        public static ConfigEntry<float> containerRange;
+        public static ConfigEntry<float> fireplaceRange;
+        public static ConfigEntry<float> smelterOreRange;
+        public static ConfigEntry<float> smelterFuelRange;
+        public static ConfigEntry<string> fuelDisallowTypes;
+        public static ConfigEntry<string> oreDisallowTypes;
+        public static ConfigEntry<KeyboardShortcut> toggleKey;
+        public static string toggleString = "Auto Fuel: {0}";
+        public static ConfigEntry<bool> refuelStandingTorches;
+        public static ConfigEntry<bool> refuelWallTorches;
+        public static ConfigEntry<bool> refuelFirePits;
+        public static ConfigEntry<bool> blastFurnaceFix;
+        public static ConfigEntry<bool> restrictKilnOutput;
+        public static ConfigEntry<int> restrictKilnOutputAmount;
+
+        public static ConfigEntry<bool> leaveLastItem;
+        public static ConfigEntry<bool> isOn;
+        public static ConfigEntry<bool> modEnabled;
+        public static ConfigEntry<bool> distributedFilling;
+        public static ConfigEntry<int> nexusID;
+        public static ConfigEntry<bool> _configVerifyClient;
+        public static ConfigEntry<bool> configStackSmelters;
+        public static ConfigEntry<bool> configBlastFurnaceTakesAll;
+        internal static readonly List<Container> ContainerList = new();
+        public static ConfigEntry<float> mRange;
+        public static ConfigEntry<bool> refuelBraziers;
+        public static ConfigEntry<bool> turnOffKiln;
+        public static ConfigEntry<bool> refuelHearth;
+        public static ConfigEntry<bool> refuelHotTub;
+        public static ConfigEntry<bool> turnOffWindmills;
+        public static ConfigEntry<bool> turnOffSpinningWheel;
+        internal static string ConnectionError = "";
+        public static bool configVerifyClient => _configVerifyClient.Value;
+        public static float lastFuel;
+        public static int fuelCount;
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource AutomaticFuelLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+        { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+
+            _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On,
+                "If on, the configuration is locked and can be changed by server admins only.");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+            _configVerifyClient = config("", "Verify Clients", true, "Disable this to turn off the client verification and version checks.");
+            dropRange = config("General", "DropRange", 15f,
+                new ConfigDescription("The maximum range to pull dropped fuel",
+                new AcceptableValueRange<float>(1f, 50f)));
+            fireplaceRange = config("Fireplace", "FireplaceRange", 5f,
+                 new ConfigDescription("The maximum range to pull fuel from containers for fireplaces",
+                 new AcceptableValueRange<float>(1f, 50f)));
+            smelterOreRange = config("Smelters", "SmelterOreRange", 15f,
+                             new ConfigDescription("The maximum range to pull fuel from containers for smelters",
+                             new AcceptableValueRange<float>(1f, 50f)));
+            smelterFuelRange = config("Smelters", "SmelterFuelRange", 15f,
+                             new ConfigDescription("The maximum range to pull ore from containers for smelters",
+                             new AcceptableValueRange<float>(1f, 50f)));
+            restrictKilnOutputAmount = config("Smelters", "RestrictKilnOutputAmount", 50,
+                new ConfigDescription("Amount of coal to shut off kiln fueling max 200",
+                new AcceptableValueRange<int>(1, 200)));
+
+
+
+
+            configStackSmelters = config("Smelters", "AllowStackSmelters", false, "Allows smelters, kiln to be stacked.  Takes away the smoke and smoke blocked check");
+            fuelDisallowTypes = config("Fireplace", "FuelDisallowTypes", "RoundLog,FineWood", "Types of item to disallow as fuel (i.e. anything that is consumed), comma-separated.");
+            oreDisallowTypes = config("Smelters", "OreDisallowTypes", "RoundLog,FineWood", "Types of item to disallow as ore (i.e. anything that is transformed), comma-separated).");
+            configBlastFurnaceTakesAll = config("Smelters", "BlastFurnaceTakesAll", true, "Allows the Blast Furnace to take all ore");
+            //toggleString = config("General", "ToggleString", "Auto Fuel: {0}", "Text to show on toggle. {0} is replaced with true/false");
+
+            toggleKey = config("General", "ToggleKey", new KeyboardShortcut(KeyCode.F10),
+                new ConfigDescription("HotKey to disable and enable AutoFuel", new AcceptableShortcuts()));
+
+            turnOffWindmills = config("Smelters", "Turn Off Windmills", false, "Turn off the Windmills");
+            turnOffSpinningWheel = config("Smelters", "Turn Off SpinningWheel", false, "Turn off the Spinnng Wheel");
+            turnOffKiln = config("Smelters", "Turn Off Kiln", false, "Turn off the Kiln");
+            refuelStandingTorches = config("Fireplace", "RefuelStandingTorches", true, "Refuel standing torches");
+            refuelBraziers = config("Fireplace", "RefuelBraziers", true, "Refuel Braziers");
+            refuelHotTub = config("Fireplace", "RefuelHotTub", true, "Refuel HotTub");
+            refuelWallTorches = config("Fireplace", "RefuelWallTorches", true, "Refuel wall torches");
+            refuelFirePits = config("Fireplace", "RefuelFirePits", true, "Refuel fire pits");
+            refuelHearth = config("Fireplace", "RefuelHearth", true, "Refuel Hearth");
+            restrictKilnOutput = config("Smelters", "RestrictKilnOutput", false, "Restrict kiln output");
+
+            isOn = config("", "IsOn", true, "Behaviour is currently on or not");
+            distributedFilling = config("Smelters", "DistributedFueling", true, "If true, refilling will occur one piece of fuel or ore at a time, making filling take longer but be better distributed between objects.");
+            leaveLastItem = config("Smelters", "LeaveLastItem", false, "Don't use last of item in chest");
+            modEnabled = config("", "Enabled", true, "Enable this mod");
+
+            if (!modEnabled.Value)
+                return;
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                AutomaticFuelLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                AutomaticFuelLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                AutomaticFuelLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+        }
+
+        #endregion
+        public static void Dbgl(string str = "", bool pref = true)
+        {
+            if (isDebug)
+                Debug.Log((pref ? typeof(AutomaticFuelPlugin).Namespace + " " : "") + str);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(toggleKey.Value.MainKey) && !TastyUtils.IgnoreKeyPresses(true))
+            {
+                isOn.Value = !isOn.Value;
+                Config.Save();
+                Player.m_localPlayer.Message(MessageHud.MessageType.Center, string.Format(toggleString, isOn.Value), 0, null);
+            }
+        }
+
+        // Container Patches
+        [HarmonyPatch(typeof(Container), nameof(Container.Awake))]
+        static class ContainerAwakePatch
+        {
+            static void Postfix(Container __instance, ZNetView ___m_nview)
+            {
+
+                TastyUtils.AddContainer(__instance, ___m_nview);
+            }
+        }
+
+        [HarmonyPatch(typeof(Container), nameof(Container.OnDestroyed))]
+        static class ContainerOnDestroyedPatch
+        {
+            static void Prefix(Container __instance)
+            {
+                ContainerList.Remove(__instance);
+            }
+        }
+
+    }
+ 
+}
